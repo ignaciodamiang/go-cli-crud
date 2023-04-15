abce5fac1d2323166441166811311d161ec806b2
Ignacio Damián González <igonzalez@startuniversity.net>
2023-04-15 01:42:03 -0300

    all

--- main.go
+++ main.go
@@ -1,6 +1,93 @@
 package main
 
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 
-func main(){
-	println("Hello World")
+	task "github.com/ignaciodamiang/go-cli-crud/tasks"
+)
+
+func main() {
+	file, err := os.OpenFile("tasks.json", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	var tasks []task.Task
+
+	info, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	if info.Size() != 0 {
+		bytes, err := io.ReadAll(file)
+		if err != nil {
+			panic(err)
+		}
+
+		err = json.Unmarshal(bytes, &tasks)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		tasks = []task.Task{}
+	}
+
+	if len(os.Args) < 2 {
+		printUsage()
+		return
+	}
+
+	switch os.Args[1] {
+	case "list":
+		task.ListTasks(tasks)
+	case "add":
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("Enter task name:")
+		name, _ := reader.ReadString('\n')
+		name = strings.TrimSpace(name)
+
+		tasks = task.AddTask(tasks, name)
+		task.SaveTasks(file, tasks)
+	case "delete":
+		if len(os.Args) < 3 {
+			fmt.Println("Must provide task ID to delete")
+			return
+		}
+		id, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println("Invalid ID")
+			return
+		}
+		tasks = task.DeleteTask(tasks, id)
+		task.SaveTasks(file, tasks)
+
+	case "complete":
+		if len(os.Args) < 3 {
+			fmt.Println("Must provide task ID to delete")
+			return
+		}
+		id, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println("Invalid ID")
+			return
+		}
+		tasks = task.CompleteTask(tasks, id)
+		task.SaveTasks(file, tasks)
+
+	default:
+		printUsage()
+	}
+}
+
+func printUsage() {
+	fmt.Println("Usage: go-cli-crud [list|add|complete|delete]")
 }
